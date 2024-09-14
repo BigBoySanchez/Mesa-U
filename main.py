1aae1ef7d331970d5750c5b468186c42bbce95a3
yoshiyahoo <josiahtroup@gmail.com>
2024-09-14 10:21:23 -0700

    clicking works, button still needs work

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pygame
 
+def initial_page(screen):
+    # fill the background first, then add stuff later
+    screen.fill("white")
+
+    # Here is a basic button
+    green_button = pygame.Surface((200, 50))
+    green_button.fill((0, 255, 0))
+    screen.blit(green_button, (550, 500))
+
+    #for event in pygame.event.get():
+    #    if green_button.get_rect().collidepoint(pos):
+    #        print("Button Clicked")
+
 
 def main():
     pygame.init()
@@ -12,9 +25,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                print("mouse clicked")
+                pos = pygame.mouse.get_pos()
 
         # Fill the screen with blue
-        screen.fill("blue")
+        initial_page(screen)
 
         pygame.display.flip()
         
