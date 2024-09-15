1f855dd6a230d3cd75a88968f301099ad2dc74a4
BigBoySanchez <jacobmath3@gmail.com>
2024-09-14 18:58:35 -0700

    added why member, body can be a surface

--- email.py
+++ email.py
@@ -4,12 +4,13 @@ import game_config
 # solution is the name of the object that was pressed
 # ex: delete, reply, link (sent from subject)
 class email:
-    def __init__(self, subject, name, address, body, solution):
+    def __init__(self, subject, name, address, body, solution, why):
         self.subject = subject
         self.name = name
         self.address = address
-        self.body = body
+        self.body = body            # surface
         self.solution = solution
+        self.why = why
 
     def check_solution(self, name):
         return name == self.solution
